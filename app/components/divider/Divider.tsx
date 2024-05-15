fdd99cae3315a25f1920ec5fc97698c44e929498
Filip Bašić <canisorpdev@gmail.com>
2024-05-15 17:25:30 +0000

    Initial commit

---
+++ app/components/divider/Divider.tsx
@@ -0,0 +1,8 @@
+export const Divider = () => (
+  //<div className="w-[95%] flex flex-row items-center gap-4 animate-fadein snap-start">
+  //  <div className="flex-1 h-0.5 bg-slate-700 bg-slate-700"></div>
+  //  <div className="flex-none w-2 h-2 rotate-45 bg-slate-700"></div>
+  //  <div className="flex-1 h-0.5 bg-slate-700 bg-slate-700"></div>
+  //</div>
+  <></>
+);
